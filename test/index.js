a7bdc310a284052e77ffa5487ba02f18896921b0
Blake Kostner <btkostner@gmail.com>
2019-07-08 13:29:36 -0600

    test: add ava test fixture

--- test/index.js
+++ test/index.js
@@ -7,38 +7,24 @@ const eslint = require('eslint')
 const path = require('path')
 const test = require('ava')
 
-const IGNORED_RULES = [
-  // Some Ava rules are impossible to test in tests
-  'ava/no-ignored-test-files',
-  'ava/no-import-test-files'
-]
-
 function lint (t, file) {
   const cli = new eslint.CLIEngine({
     useEslintrc: false,
     configFile: 'eslintrc.json'
   })
 
-  const results = cli.executeOnFiles([file]).results[0].messages
-    .filter((result) => !IGNORED_RULES.includes(result.ruleId))
+  const result = cli.executeOnFiles([file])
 
-  t.log(results)
+  result.results.map((res) => res.messages.forEach(t.log))
 
-  const errors = results.filter((res) => res.severity === 2)
-  const warnings = results.filter((res) => res.severity === 1)
-
-  t.deepEqual(errors, [], 'Non 0 error count')
-  t.deepEqual(errors, [], 'Non 0 warning count')
+  t.is(result.errorCount, 0, 'Non 0 error count')
+  t.is(result.warningCount, 0, 'Non 0 warning count')
 }
 
 test('lints javascript files correctly', (t) => {
-  return lint(t, path.join(__dirname, '_fixtures/file.js'))
-})
-
-test('lints ava test files correctly', (t) => {
-  return lint(t, path.join(__dirname, '_fixtures/test.js'))
+  return lint(t, path.join(__dirname, '_fixture.js'))
 })
 
 test('lints vue files correctly', (t) => {
-  return lint(t, path.join(__dirname, '_fixtures/file.vue'))
+  return lint(t, path.join(__dirname, '_fixture.vue'))
 })
